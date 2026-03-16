7645c2e3f0f7adb49394e8fa8faeffe30e2bb558
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:41:04 -0400

    Ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 4)   # División normal
+print(10 // 4)  # División entera (sin decimales)
+print(10 % 3)   # Módulo (residuo de la división)
+print(2 ** 4)   # Exponente (2 elevado a la 4)
