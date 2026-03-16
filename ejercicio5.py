f760b3e6778810d524b26a4a61422ca03c8d3378
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:43:35 -0400

    Ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+num1 = input("Ingresa el primer numero: ")
+num2 = input("Ingresa el segundo numero: ")
+add = int(num1) + int(num2) 
+print("La suma es: ", add)
