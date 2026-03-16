d40413d5026a78aebf0cab1476b95b046fae4fa0
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:53:50 -0400

    Ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,5 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+numbers = (1, 2, 3, 4, 5, 6)
+print(numbers[3]) # Accede al elemento en la posición 3 (imprime 4)
