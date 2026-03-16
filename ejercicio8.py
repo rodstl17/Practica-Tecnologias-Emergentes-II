4d5dc0487ccd2246294ed16351bf89ccb27c993c
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:49:57 -0400

    Ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,10 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)   # Agrega un elemento al final
+numbers.insert(0, -1)   # Inserta el -1 en la posición 0
+numbers.remove(1)   # Elimina el elemento con valor 1
+print(numbers)
+numbers.clear()  # Elimina el contenido de la lista
+print(numbers)
