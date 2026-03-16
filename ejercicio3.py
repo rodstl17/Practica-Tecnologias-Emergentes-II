b518a85bc5ea25eaf1d84429ab275be96333d5c0
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:40:09 -0400

    Ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+# Variables en Python
+course = "Python para iniciantes"
+
+print(course.upper())   # Convierte a mayúsculas
+print(course.lower())   # Convierte a minúsculas
+print(course.capitalize())  # Pone la primera letra en mayúscula
+print(course.find("h")) # Encuentra la posición de la letra "h"
+print(course.replace("para", "FOR")) # Reemplaza una palabra por otra
