2bb2218449620077d6a483fde3e04b9fb451aece
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:36:17 -0400

    Ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+# Variables en Python
+age = 20           # Entero (int)
+price = 50.5       # Flotante (float)
+name = "Bruno Diaz" # String (str)
+flag = True      # Booleano (bool)
+
+print(age, price, name, flag)
