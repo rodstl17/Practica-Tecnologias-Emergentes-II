772e5654291ab7d0129c1b31e801146deac3d43b
Rodrigo Colque <rod_styles17@outlook.es>
2026-03-16 14:45:09 -0400

    Ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,10 @@
+# Estudiante: Rodrigo Colque Poma 
+# Paralelo: 7 C 
+# Materia: Tecnologias Emergentes II
+temp = 18
+if temp > 28:
+    print("Hace calor")
+elif temp > 20:
+    print("Es un dia agradable")
+else:
+    print("Hace frio")
